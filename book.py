2f54210bcbce731351e3909a401b5564ba430451
agray998 <a.gray.4797@gmail.com>
2023-07-25 10:13:56 +0100

    basic solution

--- book.py
+++ book.py
@@ -15,18 +15,28 @@ genres and override the __init__ method and __str__ methods appropriately
 '''
 
 class Book():
+    books = []
     def __init__(self, title, pages, isbn, genre, author = "Unknown"):
-        ...
+        self.title = title
+        self.pages = pages
+        self.isbn = isbn
+        self.genre = genre
+        self.author = author
+        Book.books.append(self)
     
     @staticmethod
     def valid_isbn(isbn):
-        ...
+        digits = ''.join(isbn.split('-'))
+        if len(digits) != 13:
+            return False
+        diglist = [int(digit) for digit in digits]
+        return ((sum([diglist[i] for i in range(12) if i % 2 == 0]) + 3 * sum([diglist[i] for i in range(12) if i % 2 != 0]) + diglist[-1]) % 10) == 0
     
     @staticmethod
     def search(author):
-        ...
+        return list(filter(lambda book: book.author == author, Book.books))
 
     def __str__(self):
-        ...
+        return f"Written by {self.author}, {self.title} is a gripping {self.pages}-page {self.genre} novel"
 
 
